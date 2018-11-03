fc7afc6ec9851f151369af8e80736d4ef509d742
Gman9000 <granvillejn@aol.com>
2018-11-03 16:43:50 -0400

    Navigation and Music
    Navigation Scripts created
    Audio Management Scripts created
    Scenes Prepped for Navigation and Music

---
+++ GameOf2018/Assets/Scripts/MovementController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+    public float speed;
+    public Transform initialPosition;
+    private Rigidbody2D myRigidBody;
+    // Use this for initialization
+    void Start () {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myRigidBody.velocity = new Vector2(speed, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "resetPoint")
+        {
+            gameObject.transform.position = initialPosition.position;
+        }
+    }
+}
