90b338ed653c12d235646e25536ed2b90c585bc5
Gman9000 <granvillejn@aol.com>
2018-11-16 00:07:40 -0500

    Credits Scene Tweaks
    Credits Edited so that it scrolls from upward to downward

--- GameOf2018/Assets/Scripts/CreditMovement.cs
+++ GameOf2018/Assets/Scripts/CreditMovement.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour {
+public class CreditMovement : MonoBehaviour
+{
     public float speed;
     public Transform initialPosition;
     private Rigidbody2D myRigidBody;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         myRigidBody = GetComponent<Rigidbody2D>();
-        myRigidBody.velocity = new Vector2(speed, 0f);
+        myRigidBody.velocity = new Vector2(0f, -speed);
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "resetPoint")
