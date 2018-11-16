9bbee081bf85f443dc1dd3478f239d944d06ee18
Gman9000 <granvillejn@aol.com>
2018-11-16 01:35:19 -0500

    Creating functions to handle jumping and wall jump
    created three functions to handle jumping and wall jumping properly without having to use hitboxs

--- GameOf2018/Assets/Scripts/PlayerController.cs
+++ GameOf2018/Assets/Scripts/PlayerController.cs
@@ -9,18 +9,25 @@ public class PlayerController : MonoBehaviour
     //Moving the player
     private Rigidbody2D myRigidBody;
     public float moveSpeed;
-
     public KeyCode left;
     public KeyCode right;
+
+    //jump handling
+    public float jumpSpeed;
+    public LayerMask WhatIsGround;
+    private BoxCollider2D myCollider;
+
 
     //
     //Jump checking
     public KeyCode jump;
     public Transform groundCheck;
     public float groundCheckradius;
-    public LayerMask WhatIsGround;
+
+
+
+
     public bool isGrounded;
-    public float jumpSpeed;
     private bool doublejump;
     public int wallJumps;
     private int wallJumpTracker;
@@ -37,12 +44,13 @@ public class PlayerController : MonoBehaviour
     //private Animator myAnimator;
 
     // sound effects
-    public AudioSource throwSnowballSound;
+    //public AudioSource throwSnowballSound;
 
     // Use this for initialization
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<BoxCollider2D>();
         //myAnimator = GetComponent<Animator>();
         //theLevelManager = FindObjectOfType<LevelManager>();
         doublejump = false;
@@ -54,14 +62,14 @@ public class PlayerController : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //snowBallcounter -= Time.deltaTime;
 
         //JumpSetup
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckradius, WhatIsGround);
-        isTouchingJumpableWallOnLeft = Physics2D.OverlapCircle(wallJumpCheckLeft.position, wallCheckRadius, whatIsJumpableWall);
-        isTouchingJumpableWallOnRight = Physics2D.OverlapCircle(wallJumpCheckRight.position, wallCheckRadius, whatIsJumpableWall);
+        isGrounded = IsGrounded();
+        isTouchingJumpableWallOnLeft = IsTouchingLeftWall();
+        isTouchingJumpableWallOnRight = IsTouchingRightWall();
         //Moving the Player
         if (Input.GetKey(right))
         {
@@ -99,12 +107,22 @@ public class PlayerController : MonoBehaviour
                     myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, jumpSpeed, 0f);
                     doublejump = false;
                 }
-                else if (Input.GetKeyDown(jump) && (wallJumpTracker > 0) && (isTouchingJumpableWallOnLeft || isTouchingJumpableWallOnRight))
+                else if (Input.GetKeyDown(jump) && (wallJumpTracker > 0))
                 {
-                    myRigidBody.AddForce(new Vector2(myRigidBody.velocity.x, 2* jumpSpeed), ForceMode2D.Impulse);
+                    if (isTouchingJumpableWallOnLeft)
+                    {
+                        myRigidBody.AddForce(new Vector2(5.0f * jumpSpeed, 2.0f * jumpSpeed), ForceMode2D.Impulse);
                         //myRigidBody.velocity = new Vector3(4*jumpSpeed, jumpSpeed, 0f);
-                    doublejump = false;
-                    wallJumpTracker -= 1;
+                        //doublejump = false;
+                        wallJumpTracker -= 1;
+                    }
+                    else if (isTouchingJumpableWallOnRight)
+                    {
+                        myRigidBody.AddForce(new Vector2(-5.0f * jumpSpeed, -2.0f * jumpSpeed), ForceMode2D.Impulse);
+                        //myRigidBody.velocity = new Vector3(4*jumpSpeed, jumpSpeed, 0f);
+                        //doublejump = false;
+                        wallJumpTracker -= 1;
+                    }
 
                 }
             }
@@ -125,6 +143,76 @@ public class PlayerController : MonoBehaviour
 
         }
     }*/
+
+    public bool IsGrounded()
+    {
+        Vector2 position= myRigidBody.position;
+        Vector2 direction = Vector2.down;
+        float xRightPosition = (float)(position.x + ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yRIghtPosition = (float)(position.y - ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+        float xLeftPosition = (float)(position.x - ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yLeftPosition = (float)(position.y - ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+
+        float distance = (myCollider.size.y/2) * transform.localScale.y * 1.05f;
+        Vector2 positionRight = new Vector2 (xRightPosition, yRIghtPosition);
+        Vector2 positionLeft = new Vector2(xLeftPosition, yLeftPosition);
+
+        RaycastHit2D hitLeft = Physics2D.Raycast(positionLeft, direction, distance, WhatIsGround);
+        RaycastHit2D hitRight = Physics2D.Raycast(positionRight, direction, distance, WhatIsGround);
+        if ( (hitLeft.collider != null) || (hitRight.collider != null))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool IsTouchingLeftWall()
+    {
+        Vector2 position = myRigidBody.position;
+        Vector2 direction = Vector2.left;
+        float xTopPosition = (float)(position.x - ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yTopPosition = (float)(position.y + ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+        float xBottomPosition = (float)(position.x - ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yBottomPosition = (float)(position.y - ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+
+        float distance = (myCollider.size.x / 2) * transform.localScale.x * 1.05f;
+        Vector2 positionTop = new Vector2(xTopPosition, yTopPosition);
+        Vector2 positionBottom = new Vector2(xBottomPosition, yBottomPosition);
+        RaycastHit2D hitTop = Physics2D.Raycast(positionTop, direction, distance, whatIsJumpableWall);
+        RaycastHit2D hitBottom = Physics2D.Raycast(positionBottom, direction, distance, whatIsJumpableWall);
+        if ((hitTop.collider != null) || (hitBottom.collider != null))
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public bool IsTouchingRightWall()
+    {
+        Vector2 position = myRigidBody.position;
+        Vector2 direction = Vector2.right;
+        float xTopPosition = (float)(position.x + ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yTopPosition = (float)(position.y + ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+        float xBottomPosition = (float)(position.x + ((myCollider.size.x / 2) * transform.localScale.x * 0.95));
+        float yBottomPosition = (float)(position.y - ((myCollider.size.y / 2) * transform.localScale.y * 0.95));
+
+        float distance = (myCollider.size.x / 2) * transform.localScale.x * 1.05f;
+        Vector2 positionTop = new Vector2(xTopPosition, yTopPosition);
+        Vector2 positionBottom = new Vector2(xBottomPosition, yBottomPosition);
+        RaycastHit2D hitTop = Physics2D.Raycast(positionTop, direction, distance, whatIsJumpableWall);
+        RaycastHit2D hitBottom = Physics2D.Raycast(positionBottom, direction, distance, whatIsJumpableWall);
+        if ((hitTop.collider != null) || (hitBottom.collider != null))
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "killzone")
