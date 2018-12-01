43af6e059abf4aeadf585f3e1958fc582a9002ce
Kwan Holloway <kwansholloway@gmail.com>
2018-12-01 12:59:11 -0500

    Misc art assets and update to sandbox
    Updated my sandbox scene trying to fix parallax stuff

--- GameOf2018/Assets/Scripts/World Effects/Parallax.cs
+++ GameOf2018/Assets/Scripts/World Effects/Parallax.cs
@@ -2,33 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parallax : MonoBehaviour {
+public class Parallax : MonoBehaviour
+{
 
-	public Transform[] backgrounds;
-	private float[] parallaxScales;
-	public float smoothing;
-	private Vector3 prevCamPosition;
+    public Transform[] backgrounds;
+    private float[] parallaxScales;
+    public float smoothing;
+    private Vector3 prevCamPosition;
 
-	// Use this for initialization
-	void Start () {
-		prevCamPosition = transform.position;
+    // Use this for initialization
+    void Start()
+    {
+        prevCamPosition = transform.position;
 
-		parallaxScales = new float[backgrounds.Length];
-		for (int i = 0; i < parallaxScales.Length; i++) {
-			parallaxScales[i] = backgrounds[i].transform.position.z * -1;
-		}
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
-		for (int i = 0; i < backgrounds.Length; i++) {
-			// Get difference between new and old camera position
-			//multiply by that layer's scale and divide by smoothing to ease the transition
-			Vector3 parallaxAmount = (prevCamPosition - transform.position) * (parallaxScales[i] / smoothing);
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < parallaxScales.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].transform.position.z * -1;
+        }
+    }
 
-			backgrounds[i].position = new Vector3((backgrounds[i].position.x + parallaxAmount.x), (backgrounds[i].position.y + parallaxAmount.y), backgrounds[i].position.z);
-		}
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            // Get difference between new and old camera position
+            //multiply by that layer's scale and divide by smoothing to ease the transition
+            Vector3 parallaxAmount = (prevCamPosition - transform.position) * (parallaxScales[i] / smoothing);
 
-		prevCamPosition = transform.position;
-	}
+            backgrounds[i].position = new Vector3((backgrounds[i].position.x + parallaxAmount.x), (backgrounds[i].position.y), backgrounds[i].position.z);
+        }
+
+        prevCamPosition = transform.position;
+    }
 }
