373220446bfd4d01e7ec966eac59cfe7c0282821
Kwan Holloway <kwansholloway@gmail.com>
2018-12-01 22:35:35 -0500

    battleBridgette and Main Menu
    When the Boss's health falls below 30% the scene switches to platBridgette

---
+++ GameOf2018/Assets/Scripts/Creatures/Enemies/Boss/BossManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+
+    public string switchLevel;
+    EnemyFighter bossFighter;
+    LevelManager levelManager;
+    // Use this for initialization
+    void Start()
+    {
+        bossFighter = GetComponent<EnemyFighter>();
+        levelManager = FindObjectOfType<LevelManager>();
+
+    }
+
+    void FixedUpdate()
+    {
+        if (bossFighter.enabled)
+        {
+            if (bossFighter.HealthPercent <= 0.3f)
+            { // if Boss health less than 30%
+                levelManager.NextLevel(switchLevel);
+            }
+        }
+
+    }
+}
